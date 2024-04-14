7e7fd4b2d6762d05e806691f620eeb9909bfda0d
umesh <work.umesh12@gmail.com>
2024-04-15 00:46:03 +0530

    "signup"

--- Access/access.py
+++ Access/access.py
@@ -2,15 +2,12 @@ from flask import Flask, request, jsonify, session
 from flask_redis import FlaskRedis
 
 app = Flask(__name__)
-api = Api(app)
 app.config['SECRET_KEY'] = '\xf0?a\x9a\\\xff\xd4;\x0c\xcbHi'
-app.config['REDIS_URL'] = 'redis:localhost:6379/0'
+app.config['REDIS_URL'] = 'redis://localhost:6379/0'
 redis_client = FlaskRedis(app)
 
-
 # signup 
-
-@app.route('/signup',method=['POST'])
+@app.route('/signup',methods=['POST'])
 def signup():
     data = request.json
     username = data.get('username')
@@ -24,3 +21,7 @@ def signup():
     
     redis_client.set(username,password)
     return jsonify({'success': 'User signed up successfully'}), 201
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
