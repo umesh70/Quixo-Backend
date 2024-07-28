256952e060f9a0b3b6f6072f24e469c1e0317a27
Mansi-Bhatnagar <bhatnagarmansi.03@gmail.com>
2024-07-28 17:29:39 +0530

    Delete workspace API

--- Workspace/workspaces.py
+++ Workspace/workspaces.py
@@ -1,16 +1,17 @@
+from flask import request, jsonify, Blueprint
+from DataBase.db_config import db, User, Workspaces
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask import request, jsonify
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from DataBase.db_config import db, User,Workspaces
-from flask import request, jsonify
-from flask_jwt_extended import jwt_required
-from flask_jwt_extended import jwt_required, get_jwt_identity
-from flask import request, jsonify,Blueprint
 
-Workspace_app = Blueprint('workspace_points',__name__)
+Workspace_app = Blueprint('workspace_points', __name__)
 
-@Workspace_app.route('/create_workspace',methods=['POST'])
+
+@Workspace_app.route('/create_workspace', methods=['POST'])
 @jwt_required()
 def create_workspace():
 
@@ -20,17 +21,19 @@ def create_workspace():
     description = data['description']
 
     admin_id = get_jwt_identity()
-    user = User.query.filter_by(id = admin_id).first()
-    
+    user = User.query.filter_by(id=admin_id).first()
+
     if not user:
         return jsonify({'error': 'Invalid user'}), 404
-    
-    new_workspace = Workspaces(workspace_name= workspace_name,admin_mail=admin_mail,admin_id= admin_id,description= description)
+
+    new_workspace = Workspaces(workspace_name=workspace_name,
+                               admin_mail=admin_mail, admin_id=admin_id, description=description)
     db.session.add(new_workspace)
     db.session.commit()
     return jsonify({'message': f'Workspace {workspace_name} created successfully'}), 201
 
-@Workspace_app.route('/get_workspaces',methods = ['GET','POST'])
+
+@Workspace_app.route('/get_workspaces', methods=['GET', 'POST'])
 @jwt_required()
 def get_workspaces():
     # Query the database for all the workspaces
@@ -53,3 +56,19 @@ def get_workspaces():
         workspace_list.append(workspace_data)
 
     return jsonify(workspace_list), 200
+
+
+@Workspace_app.route('/delete_workspace', methods=['DELETE'])
+@jwt_required()
+def delete_workspace():
+    data = request.json
+    id = data["workspace_id"]
+
+    workspace = Workspaces.query.filter_by(workspace_id=id).first()
+
+    if not workspace:
+        return jsonify({'error': 'Workspace does not exist'}), 404
+
+    db.session.delete(workspace)
+    db.session.commit()
+    return jsonify({'message': 'Workspace deleted successfully'}), 200
